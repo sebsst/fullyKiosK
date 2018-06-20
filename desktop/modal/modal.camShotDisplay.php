5a1a6c70f6a53666760d175702938de72f2eae27
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-20 08:32:11 +0200

    Update modal.camShotDisplay.php

--- desktop/modal/modal.camShotDisplay.php
+++ desktop/modal/modal.camShotDisplay.php
@@ -3,5 +3,5 @@ if (!isConnect()) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
 echo init('src');
-echo '<center><img class="img-responsive" src="' . init('src') . '" /></center>';
+echo '<center><img class="img-responsive" src="' . init('src'). '&password=' . init('pwd') . '" /></center>';
 ?>
