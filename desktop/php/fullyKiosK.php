2bcf6f269cc6f9aaf583262a0a8347178c162257
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-26 16:12:27 +0200

    Update fullyKiosK.php

--- desktop/php/fullyKiosK.php
+++ desktop/php/fullyKiosK.php
@@ -110,10 +110,10 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						<label class="col-sm-3 control-label">{{Délai de rafraîchissement en minutes}}</label>
 						<div class="col-sm-3">
 						<select id="sel_object" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="refreshDelay">
-							<option value="15">{{15}}</option>
+
 							<option value="1">{{1}}</option>
 							<option value="5">{{5}}</option>
-						
+							<option value="15">{{15}}</option>						
 							<option value="30">{{30}}</option>
 							<option value="60">{{60}}</option>
 
