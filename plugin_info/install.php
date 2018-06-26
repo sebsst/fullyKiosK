bd8c0fb9b4cfc8813e58d1fd78b11884cdde3c59
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-26 21:21:35 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -19,11 +19,28 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function fullyKiosK_install() {
+  
 
 }
 
 function fullyKiosK_update() {
 
+      foreach (eqLogic::byType('fullyKiosK', false) as $eqpt) {
+        if( $eqpt->getConfiguration('refreshDelay','') == '')
+        { $eqpt->setConfiguration('refreshDelay', '15');
+          $eqpt->save();
+        }
+      // add actions if missing
+         worxLandroidS::newAction($eqpt,'off_today',$commandIn,"off_today",'other');
+         worxLandroidS::newAction($eqpt,'on_today',$commandIn,"on_today",'other');
+         worxLandroidS::newAction($eqpt,'rain_delay_0',$commandIn,"0",'other');
+         worxLandroidS::newAction($eqpt,'rain_delay_30',$commandIn,"30",'other');
+         worxLandroidS::newAction($eqpt,'rain_delay_60',$commandIn,"60",'other');
+         worxLandroidS::newAction($eqpt,'rain_delay_120',$commandIn,"120",'other');
+         worxLandroidS::newAction($eqpt,'rain_delay_240',$commandIn,"240",'other');
+ 
+    }
+  
 }
 
 
